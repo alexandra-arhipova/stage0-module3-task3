57a8855882150b2d71dfda0ce2e9e07168f52af9
alexandra-arhipova <alexandra9.arhipova@gmail.com>
2023-10-27 18:36:34 +0300

    module 3 task 3 fixed.

--- src/main/java/school/mjc/stage0/module3/task3/DeclaringVars.java
+++ src/main/java/school/mjc/stage0/module3/task3/DeclaringVars.java
@@ -2,7 +2,7 @@ package school.mjc.stage0.module3.task3;
 
 public class DeclaringVars {
     public static void main(String[] args) {
-      int a_b_c = 10;
-        System.out.println(a_b_c);
+        int a,b,c = 10;
+        System.out.println(c);
     }
 }
